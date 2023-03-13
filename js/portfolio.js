52e1342c7efdfa98659031746a3b1d8673b55ed7
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-03-13 16:08:55 +0900

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.main').fullpage({
+        anchors: ['main', 'portfolio01', 'portfolio02', 'portfolio03', 'portfolio04', 'portfolio05'],
+    })
+
+    $('.mopen').on('click', function () {
+        $(this).toggleClass('on');
+    })
+})
