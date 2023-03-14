3e0f2b7c35ff0c68a8186db205eedc414a77b937
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-03-14 12:14:46 +0900

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,35 @@
 $(function () {
-    $('.main').fullpage({
-        anchors: ['main', 'portfolio01', 'portfolio02', 'portfolio03', 'portfolio04', 'portfolio05'],
-    })
+    var fullpageOption = {
+        anchors: ['main',
+            'portfolio01',
+            'portfolio02',
+            'portfolio03',
+            'portfolio04',
+            'portfolio05'],
+        onLeave: function (origin, destination, direction) {
+            $('.gnb li').eq(destination - 1).addClass('on').siblings().removeClass('on');
+            $('.section').eq(destination - 1).addClass('on').siblings().removeClass('on');
+        },
+        afterRender: function () {
+            $('.gnb li').eq(0).addClass('on');
+            $('.section').eq(0).addClass('on');
+        }
+    }
+
+    $('.main').fullpage(fullpageOption);
 
     $('.mopen').on('click', function () {
         $(this).toggleClass('on');
+        $('.cover').toggleClass('on');
+        $('.header').toggleClass('on');
+    })
+
+    $('.cover li').on('click', function () {
+        $('.cover').removeClass('on');
+        $('.mopen').removeClass('on');
+    })
+
+    $('.cover').on('wheel', function (e) {
+        e.stopPropagation();
     })
 })
